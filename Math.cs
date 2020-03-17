0813f5e71bd930294912d9d6862d8204e172a40e
unknown <youngchul.shin@meshkorea.net>
2020-03-17 11:28:31 +0900

    초기 코드 추가

---
+++ Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BDDTest
+{
+    public interface IMath
+    {
+        double Add(double x, double y);
+        double Sub(double x, double y);
+        double Mul(double x, double y);
+        double Div(double x, double y);
+    }
+
+    public class Math : IMath
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Div(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(y));
+            }
+
+            return x / y;
+        }
+
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
